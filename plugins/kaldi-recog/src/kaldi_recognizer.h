42887b6c6e0e5f660c40fd2ead09f23bbaea1918
Nickolay Shmyrev <nshmyrev@gmail.com>
2020-06-13 23:50:22 +0200

    Update to latest vosk-api code

--- plugins/kaldi-recog/src/kaldi_recognizer.h
+++ plugins/kaldi-recog/src/kaldi_recognizer.h
@@ -1,3 +1,17 @@
+// Copyright 2019 Alpha Cephei Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include "base/kaldi-common.h"
 #include "util/common-utils.h"
 #include "fstext/fstext-lib.h"
@@ -11,25 +25,45 @@
 #include "nnet3/nnet-utils.h"
 
 #include "model.h"
+#include "spk_model.h"
 
 using namespace kaldi;
 
 class KaldiRecognizer {
     public:
-        KaldiRecognizer(Model &model);
+        KaldiRecognizer(Model *model, float sample_frequency);
+        KaldiRecognizer(Model *model, SpkModel *spk_model, float sample_frequency);
+        KaldiRecognizer(Model *model, float sample_frequency, char const *grammar);
         ~KaldiRecognizer();
-        void CleanUp();
         bool AcceptWaveform(const char *data, int len);
-        std::string Result();
-        std::string PartialResult();
+        bool AcceptWaveform(const short *sdata, int len);
+        bool AcceptWaveform(const float *fdata, int len);
+        const char* Result();
+        const char* FinalResult();
+        const char* PartialResult();
 
     private:
-        Model &model_;
-        SingleUtteranceNnet3Decoder *decoder_;
+        void InitRescoring();
+        void CleanUp();
+        void UpdateSilenceWeights();
+        bool AcceptWaveform(Vector<BaseFloat> &wdata);
+        void GetSpkVector(Vector<BaseFloat> &xvector);
 
-        OnlineNnet2FeaturePipelineInfo *feature_info_;
+        Model *model_;
+        SingleUtteranceNnet3Decoder *decoder_;
+        fst::LookaheadFst<fst::StdArc, int32> *decode_fst_;
+        fst::StdVectorFst *g_fst_; // dynamically constructed grammar
         OnlineNnet2FeaturePipeline *feature_pipeline_;
         OnlineSilenceWeighting *silence_weighting_;
 
+        SpkModel *spk_model_;
+        OnlineBaseFeature *spk_feature_;
+
+        fst::MapFst<fst::StdArc, kaldi::LatticeArc, fst::StdToLatticeMapper<kaldi::BaseFloat> > *lm_fst_;
+
+        float sample_frequency_;
+        int32 frame_offset_;
+        int32 round_offset_;
         bool input_finalized_;
+        string last_result_;
 };
