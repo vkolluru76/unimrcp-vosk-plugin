d3e4c9855c3b0684f67c85d362ea8d90db9fc221
Nickolay Shmyrev <nshmyrev@gmail.com>
2021-01-11 16:52:44 +0100

    Update to latest vosk 0.3.17

--- plugins/vosk-recog/src/model.h
+++ plugins/vosk-recog/src/model.h
@@ -42,6 +42,7 @@ public:
     Model(const char *model_path);
     void Ref();
     void Unref();
+    int FindWord(const char *word);
 
 protected:
     ~Model();
@@ -63,6 +64,7 @@ protected:
     string std_fst_rxfilename_;
     string final_ie_rxfilename_;
     string mfcc_conf_rxfilename_;
+    string global_cmvn_stats_rxfilename_;
 
     kaldi::OnlineEndpointConfig endpoint_config_;
     kaldi::LatticeFasterDecoderConfig nnet3_decoding_config_;
