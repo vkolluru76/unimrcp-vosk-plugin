9761d95371d7a7def11a3137a3b396f806b3a2ec
vkolluru <vkolluru@getmindful.com>
2022-09-30 07:17:08 -0700

    Fixing the vosk instance with issue with silence

--- plugins/vosk-recog/src/vosk_recog_engine.c
+++ plugins/vosk-recog/src/vosk_recog_engine.c
@@ -612,7 +612,7 @@ static apt_bool_t vosk_recog_stream_write(mpf_audio_stream_t *stream, const mpf_
 		mpf_detector_event_e det_event = mpf_activity_detector_process(recog_channel->detector,frame);
 		switch(det_event) {
 			case MPF_DETECTOR_EVENT_ACTIVITY:
-				if (frame->codec_frame & frame->codec_frame.size	> 0) {
+				if (frame->codec_frame != NULL & frame->codec_frame.size > 0) {
 				    apt_log(RECOG_LOG_MARK,APT_PRIO_INFO,"Detected Voice Activity " APT_SIDRES_FMT,
                     					MRCP_MESSAGE_SIDRES(recog_channel->recog_request));
                     vosk_recog_start_of_input(recog_channel);
